6ec5b6a02f76be08043f04ee23f181d03bf79d86
Benedict Roth <benedict-roth@live.de>
2017-10-23 12:50:56 +0200

    Created Toolbar for Emote Selector.

--- app/src/main/java/aume/htwg/emoteapp/EmoteSelector.java
+++ app/src/main/java/aume/htwg/emoteapp/EmoteSelector.java
@@ -9,5 +9,6 @@ public class EmoteSelector extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emote_selector);
+
     }
 }
