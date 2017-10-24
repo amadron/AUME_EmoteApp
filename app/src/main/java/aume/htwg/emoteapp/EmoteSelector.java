e93cbf914ab3a8e858ac8019f79920787be270b8
Benedict Roth <benedict-roth@live.de>
2017-10-24 12:14:15 +0200

    Created Toolbar for Emote Selector.

--- app/src/main/java/aume/htwg/emoteapp/EmoteSelector.java
+++ app/src/main/java/aume/htwg/emoteapp/EmoteSelector.java
@@ -1,7 +1,10 @@
 package aume.htwg.emoteapp;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toolbar;
 
 public class EmoteSelector extends AppCompatActivity {
 
@@ -9,6 +12,10 @@ public class EmoteSelector extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emote_selector);
-
+        /*
+        ActionBar ab = getSupportActionBar();
+        View men = (View) getLayoutInflater().inflate(R.layout.emote_selector_bar, null);
+        ab.setCustomView(men);
+        */
     }
 }
