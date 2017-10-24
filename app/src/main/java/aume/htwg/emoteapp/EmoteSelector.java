956bc7c26b80f99476db29bd979ad7411b5dc99d
tabuechn <tabuechn@htwg-konstanz.de>
2017-10-24 13:08:09 +0200

    text for slider added

--- app/src/main/java/aume/htwg/emoteapp/EmoteSelector.java
+++ app/src/main/java/aume/htwg/emoteapp/EmoteSelector.java
@@ -1,10 +1,14 @@
 package aume.htwg.emoteapp;
 
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toolbar;
 
 public class EmoteSelector extends AppCompatActivity {
@@ -13,6 +17,29 @@ public class EmoteSelector extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emote_selector);
+        SeekBar seekBar = (SeekBar) findViewById(R.id.modeBaar);
+        final TextView seekBarValue = (TextView)findViewById(R.id.modeBarText);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress,
+                                          boolean fromUser) {
+                // TODO Auto-generated method stub
+                seekBarValue.setText(String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+                seekBar.getProgressDrawable().setColorFilter(Color.RED, PorterDuff.Mode.SRC_IN);
+            }
+        });
     }
 
     public void goBack(View view)
